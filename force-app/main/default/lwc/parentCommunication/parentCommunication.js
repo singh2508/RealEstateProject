157f4e16a57887f743a89e0ddff1f5ec8eee9d59
Prashant Ranjan <prashantranjan249@gmail.com>
2026-06-25 17:09:29 +0530

    Added parent to child communication example

--- force-app/main/default/lwc/parentCommunication/parentCommunication.js
+++ force-app/main/default/lwc/parentCommunication/parentCommunication.js
@@ -12,6 +12,9 @@ export default class ParentCommunication extends LightningElement {
         let childArray=event.detail
         childArray.forEach(item =>console.log(item)); // for whole data print
         console.log(childArray[2]); //for single data print 
+        git add .
+        git commit -m "Added parent to child communication example"
+        git push
 
      */
     this.name=event.detail;
